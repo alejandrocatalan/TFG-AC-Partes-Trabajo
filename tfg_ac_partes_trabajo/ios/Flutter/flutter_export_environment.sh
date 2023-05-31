cbc350adaf4dd994becc58b759bb995677916f78
Alejandro Catalan <a2577@inycom.es>
2023-05-31 17:41:45 +0200

    add proyecto en repositorio github

---
+++ tfg_ac_partes_trabajo/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/alejandrocatalantejedor/Development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/alejandrocatalantejedor/Development/TFG_AC_PartesDeTrabajo/tfg_ac_partes_trabajo"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/alejandrocatalantejedor/Development/TFG_AC_PartesDeTrabajo/tfg_ac_partes_trabajo/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=0.1.0"
+export "FLUTTER_BUILD_NUMBER=0.1.0"
+export "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/alejandrocatalantejedor/Development/TFG_AC_PartesDeTrabajo/tfg_ac_partes_trabajo/.dart_tool/package_config.json"
